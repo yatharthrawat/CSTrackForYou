981112067e99e77c65611220da2878d776f5d1f5
yatharthrawat <rawat.yatharth@gmail.com>
2018-05-16 01:01:47 +0530

    Added Course Buttons without onclick event

--- src/App.js
+++ src/App.js
@@ -1,7 +1,91 @@
 import React from 'react';
 import './App.css';
 
-class App extends React.Component {
+
+const courselist=[
+    "MA 266",
+    "MA 366",
+    "CS 314",
+    "CS 307",
+    "CS 334",
+    "CS 352",
+    "CS 354",
+    "CS 381",
+    "CS 434",
+    "CS 348",
+    "CS 448",
+    "CS 471",
+    "CS 490",
+    "CS 514",
+    "CS 515",
+    "CS 497",
+    "CS 456",
+    "CS 483",
+    "CS 422",
+    "CS 373",
+    "CS 473",
+    "CS 355",
+    "CS 426",
+    "CS 478",
+    "CS 390-WAP",
+    "CS 497",
+    "STAT 416",
+    "MA 416",
+    "STAT 512",
+    "CS 353",
+    "CS 408",
+    "CS 489",
+    "CS 490-DS0",
+    "CS 490-SWS",
+    "CS 407",
+    "CS 390-VRA",
+    "CS 490-Senior Project"];
+
+function Course(props) {
+    return (
+        <button className="course">
+            {props.value}
+        </button>
+    );
+}
+
+
+class ButtonInterface extends React.Component{
+    renderCourse(i) {
+        return (
+            <Course value={courselist[i]}
+            />
+        );
+    }
+    render(){
+        var buttons=[];
+        for(var i=0;i<courselist.length;i++){
+            buttons.push(this.renderCourse(i));
+
+        }
+        return(
+            <div>
+                <div>
+                <h1 className="App-title">CS Track for You</h1>
+                </div>
+                <div className="Graph">
+                <Graph/>
+                </div>
+                <div className="buttonrow">
+                    {buttons}
+                </div>
+
+            </div>
+
+        );
+    }
+}
+
+
+
+
+
+class Graph extends React.Component {
     constructor(props){
         super(props);
         this.state={CSE:100,CGV:50,DIS:5,FCS:5,ML:13,PL:43,SEC:0,SE:0,SP:0};
@@ -33,27 +117,13 @@ class App extends React.Component {
       };
 
 
-      let options= {
-          scales: {
-              yAxes: [{
-                  ticks: {
-                      beginAtZero:true
-                  }
-              }],
-                  xAxes: [{
-                  ticks: {
-                      fontSize: 1000
-                  }
-              }]
-          }
-      }
       let BarChart = require("react-chartjs").Bar;
 
-      return (<div>
-          <br/><br/><br/><br/>
-          <BarChart height="600" width="600" data={data} options={options}/>
-      </div>);
+      return (
+          <BarChart height="600" width="1200" data={data}/>);
   }
 }
 
-export default App;
+
+
+export default ButtonInterface;
